c3c81235dddadf41b936930f9462fe6682ebdc60
AnshikaDixit <dixitanshika09@gmail.com>
2024-09-16 09:55:35 +0530

    ADDED --> Home and routing for few pages

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render, HttpResponse
 
 # Create your views here.
-def home(request):
-    return HttpResponse("This is home")
+def blogHome(request):
+    return HttpResponse("This is blog home")
+
+def blogPost(request, slug):
+    return HttpResponse(f"This is blog post : {slug}")
+
